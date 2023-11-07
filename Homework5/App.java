3a38acd53e29a0735c451fa0ecc9a6f3d6ec84e5
Alexandr <a.a.shutov@bk.ru>
2023-11-07 11:48:05 +0300

    Homework5

--- Homework5/App.java
+++ Homework5/App.java
@@ -1,15 +1,30 @@
+/*
+Есть пять философов (потоки), которые могут либо обедать либо размышлять.
+Каждый философ должен пообедать три раза. Каждый прием пищи длиться 500 миллисекунд
+После каждого приема пищи философ должен размышлять
+Не должно возникнуть общей блокировки
+Философы не должны есть больше одного раза подряд
+ */
 
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
 
     public static void main(String[] args) {
 
-        Thread currentThread = Thread.currentThread();
-        ThreadGroup threadGroup = currentThread.getThreadGroup();
-        System.out.println("Thread: " + currentThread.getName());
-        System.out.println("Thread Group: " + threadGroup.getName());
-        System.out.println("Parent Group: " + threadGroup.getParent().getName());
+        List<Philosopher> philosophers = new ArrayList<>();
+        Philosopher.philosophers = philosophers;
 
+        for (int i = 0; i < 5; i++) {
+            philosophers.add(new Philosopher());
+        }
+
+        for (Philosopher philosopher: philosophers) {
+            philosopher.getThread().start();
+        }
 
     }
+
 }
